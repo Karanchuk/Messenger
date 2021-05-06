2c72a57872a963faf3cadc1c9daeb56c1aa98d48
bohdan.karanchuk <b.karanchuk@gmail.com>
2021-05-06 10:39:24 +0300

    Сделаны все пункты задания

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
 public class Controller {
+
+    @FXML
+    TextArea textArea;
+    @FXML
+    TextField textField;
+
+    public void sendBtnClick() {
+        appendToTextArea();
+        textField.requestFocus();
+    }
+
+    public void textFieldEnterPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER) {
+            appendToTextArea();
+        }
+    }
+
+    private void appendToTextArea() {
+        if (!textField.getText().isEmpty()) {
+            textArea.appendText((textArea.getText().isEmpty() ? "" : "\n") + textField.getText());
+            textField.clear();
+        }
+    }
 }
